b4a9f7c431b84ffe584c4b5cb33f5590d5e8e834
Victor Gabou <victor.gabou@outlook.fr>
2018-10-12 00:56:08 +0200

    add blog capabilities

--- wp-content/themes/wordpress-vuejs/custom-post-types.php
+++ wp-content/themes/wordpress-vuejs/custom-post-types.php
@@ -1,1 +1,96 @@
 <?php
+
+// Register Custom Post Type Blog
+// Post Type Key: blog
+function create_blog_cpt() {
+
+	$labels = array(
+		'name' => __( 'Blogs', 'Post Type General Name', 'textdomain' ),
+		'singular_name' => __( 'Blog', 'Post Type Singular Name', 'textdomain' ),
+		'menu_name' => __( 'Blogs', 'textdomain' ),
+		'name_admin_bar' => __( 'Blog', 'textdomain' ),
+		'archives' => __( 'Blog Archives', 'textdomain' ),
+		'attributes' => __( 'Blog Attributes', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent Blog:', 'textdomain' ),
+		'all_items' => __( 'All Blogs', 'textdomain' ),
+		'add_new_item' => __( 'Add New Blog', 'textdomain' ),
+		'add_new' => __( 'Add New', 'textdomain' ),
+		'new_item' => __( 'New Blog', 'textdomain' ),
+		'edit_item' => __( 'Edit Blog', 'textdomain' ),
+		'update_item' => __( 'Update Blog', 'textdomain' ),
+		'view_item' => __( 'View Blog', 'textdomain' ),
+		'view_items' => __( 'View Blogs', 'textdomain' ),
+		'search_items' => __( 'Search Blog', 'textdomain' ),
+		'not_found' => __( 'Not found', 'textdomain' ),
+		'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+		'featured_image' => __( 'Featured Image', 'textdomain' ),
+		'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+		'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+		'insert_into_item' => __( 'Insert into Blog', 'textdomain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Blog', 'textdomain' ),
+		'items_list' => __( 'Blogs list', 'textdomain' ),
+		'items_list_navigation' => __( 'Blogs list navigation', 'textdomain' ),
+		'filter_items_list' => __( 'Filter Blogs list', 'textdomain' ),
+	);
+	$args = array(
+		'label' => __( 'Blog', 'textdomain' ),
+		'description' => __( '', 'textdomain' ),
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-edit',
+		'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author', 'comments', 'trackbacks', 'page-attributes', 'post-formats', 'custom-fields', ),
+		'taxonomies' => array( 'blog_category' ),
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'hierarchical' => false,
+		'exclude_from_search' => false,
+		'show_in_rest' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
+	);
+	register_post_type( 'blog', $args );
+
+}
+add_action( 'init', 'create_blog_cpt', 0 );
+
+// Register Taxonomy Category
+// Taxonomy Key: category
+function create_category_tax() {
+
+	$labels = array(
+		'name'              => _x( 'Categories', 'taxonomy general name', 'textdomain' ),
+		'singular_name'     => _x( 'Category', 'taxonomy singular name', 'textdomain' ),
+		'search_items'      => __( 'Search Categories', 'textdomain' ),
+		'all_items'         => __( 'All Categories', 'textdomain' ),
+		'parent_item'       => __( 'Parent Category', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent Category:', 'textdomain' ),
+		'edit_item'         => __( 'Edit Category', 'textdomain' ),
+		'update_item'       => __( 'Update Category', 'textdomain' ),
+		'add_new_item'      => __( 'Add New Category', 'textdomain' ),
+		'new_item_name'     => __( 'New Category Name', 'textdomain' ),
+		'menu_name'         => __( 'Categories', 'textdomain' ),
+	);
+	$args = array(
+		'labels' => $labels,
+		'description' => __( '', 'textdomain' ),
+		'hierarchical' => false,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'show_in_nav_menus' => true,
+		'show_in_rest' => false,
+		'show_tagcloud' => true,
+		'show_in_quick_edit' => true,
+		'show_admin_column' => false,
+	);
+	register_taxonomy( 'blog_category', array('blog', ), $args );
+
+}
+add_action( 'init', 'create_category_tax' );
