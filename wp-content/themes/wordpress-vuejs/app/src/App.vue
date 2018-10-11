134e6b3192d2752e168358fde47f9e91c985ba92
Victor Gabou <victor.gabou@outlook.fr>
2018-10-12 01:31:33 +0200

    header as component

--- wp-content/themes/wordpress-vuejs/app/src/App.vue
+++ wp-content/themes/wordpress-vuejs/app/src/App.vue
@@ -1,41 +1,28 @@
 <template>
   <div id="app">
-    <header>
-      <ul>
-        <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
-        <li v-for="item in header" :key="item.id">
-          <router-link :to="{ path: item.url }">{{ item.title }}</router-link>
-        </li>
-      </ul>
-    </header>
+    <Header />
     <router-view/>
     <PageTransitions name="fade" />
   </div>
 </template>
 
 <script>
+import Header from '@/components/layout/Header.vue'
 import PageTransitions from '@/components/layout/PageTransitions.vue'
-import { menus } from '@/services'
 export default {
   name: 'App',
   components: {
+    Header,
     PageTransitions
   },
   data () {
     return {
       transitionContainer: undefined,
-      loading: true,
-      header: []
+      loading: true
     }
   },
   mounted () {
     this.transitionContainer = document.getElementById('transition')
-    this.getHeader()
-  },
-  methods: {
-    getHeader: async function () {
-      this.header = await menus.get('header')
-    }
   }
 }
 </script>
---
+++ wp-content/themes/wordpress-vuejs/app/src/components/layout/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <ul>
+      <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+      <li v-for="item in header" :key="item.id">
+        <router-link :to="{ path: item.url }">{{ item.title }}</router-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+import { menus } from '@/services'
+export default {
+  name: 'Header',
+  data () {
+    return {
+      header: []
+    }
+  },
+  mounted () {
+    this.getHeader()
+  },
+  methods: {
+    getHeader: async function () {
+      this.header = await menus.get('header')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
